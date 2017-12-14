5093bf8cfb9442fef5dfeb855cb54a99d1cf7bd1
wulin <125906088@qq.com>
2017-12-14 18:01:34 +0800

    datax elasticsearch reader commit

---
+++ datax-esreader/src/main/java/com/alibaba/datax/plugin/reader/esreader/Key.java
@@ -0,0 +1,54 @@
+package com.alibaba.datax.plugin.reader.esreader;
+
+public final class Key {
+	
+	/*
+	 * @name:  esClusterName
+	 * @description:  elastic search cluster name
+	*/
+	public final static String esClusterName = "esClusterName";
+	
+	/*
+	 * @name:  esClusterIP
+	 * @description:  elastic search cluster ip
+	*/
+	public final static String esClusterIP = "esClusterIP";
+	
+	/*
+	 * @name:  esClusterPort
+	 * @description:  elastic search cluster port
+	*/
+	public final static String esClusterPort = "esClusterPort";
+	
+	/*
+	 * @name: esIndex 
+	 * @description:  elastic search index
+	 */
+	public final static String esIndex = "esIndex";
+	
+	/*
+	 * @name: esType
+	 * @description:  elastic search type
+	 */
+	public final static String esType = "esType";
+	
+	/**
+	 * @name: esFieldInclude
+	 * @description:  elastic search include field
+	 */
+	public final static String esFieldInclude = "esFieldInclude";
+	
+	/*
+	 * @name: batchSize
+	 * @description: elasticsearch batch size
+	 */
+	public final static String batchSize = "batchSize";
+	
+	/*
+	 * @name: readSize
+	 * @description: elasticsearch read size
+	 */
+	public final static String readSize = "readSize";
+	
+}
+
